855ca3c73c102f9e8939d1f619c913e5da89d117
Kabir <38161769+KabirKr@users.noreply.github.com>
2023-12-08 00:25:35 +0530

    fix: use webhook

--- src/bot.ts
+++ src/bot.ts
@@ -1,34 +1,35 @@
 import TelegramBot from "node-telegram-bot-api"
 import validateJsonInput from "./jsonValidation"
 
-const BOT_TOKEN = process.env.BOT_API_TOKEN as string
+export function startBot(bot: TelegramBot) {
+  bot.on("message", (msg) => {
+    const chatId = msg.chat.id
 
-const bot = new TelegramBot(BOT_TOKEN, { polling: true })
+    if (msg.text === "/start") {
+      bot.sendMessage(chatId, `Hello! ${msg.chat.first_name} 👋`)
+      return
+    }
 
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id
+    if (!validateJsonInput(msg.text)) {
+      bot.sendMessage(
+        chatId,
+        "Sorry! I don't know how to answer your message 🤔",
+      )
+      return
+    }
 
-  if (msg.text === "/start") {
-    bot.sendMessage(chatId, `Hello! ${msg.chat.first_name} 👋`)
-    return
-  }
+    // Parse quizzes and create poll
+    const quizzes = JSON.parse(msg.text as string)
 
-  if (!validateJsonInput(msg.text)) {
-    bot.sendMessage(chatId, "Sorry! I don't know how to answer your message 🤔")
-    return
-  }
+    quizzes.forEach((quiz: any) => {
+      const question = quiz.question
+      const options = quiz.options.map((option: any) => option.value)
+      const answer = quiz.answer - 1
 
-  // Parse quizzes and create poll
-  const quizzes = JSON.parse(msg.text as string)
-
-  quizzes.forEach((quiz: any) => {
-    const question = quiz.question
-    const options = quiz.options.map((option: any) => option.value)
-    const answer = quiz.answer - 1
-
-    bot.sendPoll(chatId, question, options, {
-      type: "quiz",
-      correct_option_id: answer,
+      bot.sendPoll(chatId, question, options, {
+        type: "quiz",
+        correct_option_id: answer,
+      })
     })
   })
-})
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,32 @@
 import "dotenv/config"
 import express from "express"
 import helmet from "helmet"
-import "./bot"
+import TelegramBot from "node-telegram-bot-api"
+
+import { startBot } from "./bot"
+
+const PORT = process.env.PORT || 8000
+const BOT_TOKEN = process.env.BOT_TOKEN as string
+const APP_URL = process.env.APP_URL as string
 
 const app = express()
-
-// app.use(express.json())
+app.use(express.json())
 app.use(helmet())
 
-app.listen(8000, () => {
-  console.log(`App is listening at http://localhost:${8000}`)
+// Bot setup
+const bot = new TelegramBot(BOT_TOKEN, { polling: false })
+bot.setWebHook(`${APP_URL}/bot${BOT_TOKEN}`)
+startBot(bot)
+
+app.get("/", (req, res) => {
+  return res.send("Hello!")
+})
+
+app.post(`/bot${BOT_TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body)
+  res.sendStatus(200)
+})
+
+app.listen(PORT, () => {
+  console.log(`App is listening at http://localhost:${PORT}`)
 })
